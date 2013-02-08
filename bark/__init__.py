7efc1567557735c01dd12d7f2b7be035e8022dd3
Matthew Moss <mdm@cse.unsw.edu.au>
2013-02-08 04:46:12 -0800

    Add to app setup code

--- bark/__init__.py
+++ bark/__init__.py
@@ -1,13 +1,21 @@
 from flask import Flask
+
+from flask.ext.sqlalchemy import SQLAlchemy
+
 import default_settings
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(default_settings)
     
-    #from auth import bp_auth
+    from auth import bp_auth
     #from swipe import swipe
 
-    #app.register_blueprint(bp_auth)
+    app.register_blueprint(bp_auth)
     #app.register_blueprint(swipe)
+
+    db.init_app(app)
+
     return app
