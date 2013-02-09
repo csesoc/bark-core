72e958c536ddb3972f28382a4af83a4768679574
Evgeny Martynov <evgeny@epochfail.com>
2013-02-09 13:06:44 +1100

    Move Session into models.py

--- bark/auth.py
+++ bark/auth.py
@@ -1,28 +1,12 @@
 # vim: expandtab:ts=4:sw=4
 
-from datetime import datetime, timedelta
-import os
-
 from flask import Blueprint
 
 from bark import db, user
 from bark.api import BarkApiEndpoint
+from bark.models import Session
 
 bp_auth = Blueprint("bp_auth", __name__)
-
-auth_token_length = 32
-session_timeout = 24
-
-class Session(db.Model):
-    __tablename__ = 'sessions'
-    auth_token = db.Column(db.String(auth_token_length*2), primary_key=True)
-    user = db.Column(db.Integer)
-    create_time = db.Column(db.DateTime)
-
-    def __init__(self, user):
-        self.user = user
-        self.create_time = datetime.utcnow()
-        self.auth_token = os.urandom(auth_token_length).encode('hex')
 
 class LoginView(BarkApiEndpoint):
     required_fields_ = {
@@ -65,14 +49,3 @@ bp_auth.add_url_rule(
     "/logout",
     view_func=LogoutView.as_view("logout"),
     methods=["POST"])
-
-def get_user_id(auth_token):
-    s = Session.query.filter_by(auth_token=auth_token).first()
-    if s:
-        if s.create_time + timedelta(hours=session_timeout) > datetime.utcnow():
-            return s.user
-        else:
-            db.session.delete(s)
-            return None 
-    else:
-        return None
