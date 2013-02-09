1361a7ce1df49d724353e85cce7d1202573e341d
Matthew Moss <mdm@cse.unsw.edu.au>
2013-02-09 03:20:28 -0800

    Update __init__ to use app factory pattern

--- bark/__init__.py
+++ bark/__init__.py
@@ -1,20 +1,30 @@
-from flask import Flask
-
+from flask import Flask, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
+from werkzeug.exceptions import default_exceptions
+from werkzeug.exceptions import HTTPException
 
 import default_settings
 
 db = SQLAlchemy()
 
+__all__ = ['create_app']
+
+def make_json_error(ex):
+   response = jsonify(message=str(ex))
+   response.status_code = (ex.code if isinstance(ex, HTTPException)
+                           else 500)
+   return response
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(default_settings)
-    
-    from auth import bp_auth
-    #from swipe import swipe
 
+    for code in default_exceptions.iterkeys():
+        app.error_handler_spec[None][code] = make_json_error
+
+    # Component imports. Must be here to fix cyclical problems
+    from auth import bp_auth
     app.register_blueprint(bp_auth)
-    #app.register_blueprint(swipe)
 
     db.init_app(app)
 
