7d94e09028a4691869152f9dd8d691fb11cb2b64
Matthew Moss <mdm@cse.unsw.edu.au>
2013-02-09 22:15:46 -0800

    Add JSON type conversion to BarkApiEndpoint

--- bark/lib/api.py
+++ bark/lib/api.py
@@ -6,6 +6,7 @@ from flask.views import View
 class BarkApiEndpoint(View):
     def bad_request(self, error):
         # Ignoring the error for now.
+        print error
         abort(400)
         raise Exception("wtf")  # Never reached.
 
@@ -16,13 +17,22 @@ class BarkApiEndpoint(View):
 
         method = request.method.lower()
 
-        # Verify that correct fields are present.
-        for field in self.required_fields_.get(method, []):
-            if field not in request.json:
-                self.bad_request("Missing required field '%s'" % field)
+        # Verify that correct fields are present, and of the correct type
+
+        parsed_json={}
+
+        data_types = self.required_fields_.get(method, {})
+        for data_type in data_types:
+            for field in data_types[data_type]:
+                if field not in request.json:
+                    self.bad_request("Missing required field '%s'" % field)
+                try:
+                    parsed_json[field] = data_type(request.json[field])
+                except ValueError:
+                    self.bad_request("Invalid field '%s'" % field)
 
         try:
-            response = getattr(self, method)(request.json)
+            response = getattr(self, method)(parsed_json)
         except:
             traceback.print_exc(file=sys.stdout)
             response = {
