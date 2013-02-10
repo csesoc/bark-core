647faa61dd432f1ca4d9cb44b4987c2e167efb78
Matthew Moss <mdm@cse.unsw.edu.au>
2013-02-09 23:13:46 -0800

    Finish Event creation

--- bark/events/events.py
+++ bark/events/events.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from bark import db
 from bark.lib.api import BarkApiEndpoint
 from bark.events.models import Event
@@ -7,16 +9,32 @@ from bark.auth.models import Session
 
 class CreateEventView(BarkApiEndpoint):
     required_fields_ = {
-        "post": ["auth_token", "group_id", "name", 
-                 "description", "start_time", "end_time"],
+        "post": {str: ["auth_token", "description", "name"],
+                 int: ["start_time", "end_time", "group_id"]},
     }
 
     def post(self, json):
         user = Session.get_user(auth_token)
-
-        group = Group.by_id(json["group_id"])
+        group_id = json["group_id"]
+        group = Group.by_id(group_id)
 
         if user in group.members:
-            try:
-                start_time = int(json["start_time"])
-                end_time = int(json["end_time"])
+            name = json["name"]
+            description = json["name"]
+            start_time = json["start_time"]
+            end_time = json["end_time"]
+
+            event = Event(group_id, name, description, start_time, end_time)
+            db.session.add(event)
+            db.session.commit()
+
+            return {
+                "status": "OK",
+                "event_id":, event.event_id,
+            }
+        else:
+            return {
+                "status": "REQUEST_DENIED",
+                "error_detail": "User not member of group",
+            }
+            
