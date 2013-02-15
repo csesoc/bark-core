0a223eff5f39915369076596c600fe707541a3e5
Evgeny Martynov <evgeny@epochfail.com>
2013-02-15 20:45:24 +1100

    Change the way Bark API classes handle bad requests, etc.

--- bark/auth/shared.py
+++ bark/auth/shared.py
@@ -1,34 +1,28 @@
-from flask import jsonify, request
+from flask import jsonify
 
-from bark.lib.api import BarkApiEndpoint
+from bark.lib.api import BarkApiEndpoint, BarkApiException
 from .models import Session
 
-auth_spec = {
-    "post": [
-        ("auth_token", str),
-    ],
-}
+auth_spec = [
+    ("auth_token", str),
+]
+
+class AuthenticationException(BarkApiException):
+    def status(self): return "UNAUTHORISED"
 
 class BarkAuthenticatedApiEndpoint(BarkApiEndpoint):
     """
     Checks for authentication token in the request, verifies its validity,
     stores the current user, and proceeds as normal.
-
-    Calls out to bad_request if authentication token is invalid.
     """
 
-    def dispatch_request(self):
-        method = request.method.lower()
-        self.verify_json(request.json, auth_spec[method])
+    def verify_request(self, request):
+        self.verify_json(request.json, auth_spec)
 
         auth_token = request.json["auth_token"]
         self.user = Session.get_user(auth_token)
 
         if self.user is None:
-            return jsonify({
-                "status": "REQUEST_DENIED",
-                "error_detail": "Invalid auth_token",
-            })
+            raise AuthenticationException("Invalid auth_token")
 
-        # Everything looks good at this point.
-        return super(BarkAuthenticatedApiEndpoint, self).dispatch_request()
+        return super(BarkAuthenticatedApiEndpoint, self).verify_request(request)
