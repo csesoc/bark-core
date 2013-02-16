f2ef86990b5944a729c83d172eb3f58691a3ec05
Matthew Moss <mdm@cse.unsw.edu.au>
2013-02-16 08:03:00 -0800

    Fix apostrophe

--- make_db.py
+++ make_db.py
@@ -13,7 +13,7 @@ app = create_app()
 # Someone tell me this is the right way to do it.
 # Basically, it needs an app.request_context to do db.session calls.
 # Normal app.request_context() takes in a WSGI environ object (like a dict),
-# meaning that we"d have to fake 9001 params in it.
+# meaning that we'd have to fake 9001 params in it.
 # This one works by creating a request_context for the unittests.
 #
 # tl;dr: hacks.
