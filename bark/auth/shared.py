f6a43fa92611fbfded38698f3323168b364349fe
Evgeny Martynov <evgeny@epochfail.com>
2013-02-17 20:29:31 +1100

    Remove auth_token from incoming JSON in BarkAuthenticatedApiEndpoint

--- bark/auth/shared.py
+++ bark/auth/shared.py
@@ -14,13 +14,19 @@ class BarkAuthenticatedApiEndpoint(BarkApiEndpoint):
     """
     Checks for authentication token in the request, verifies its validity,
     stores the current user, and proceeds as normal.
+
+    Adds to instance:
+      * auth_token: token from request. Removes it from request JSON.
+      * user: authenticated user.
     """
 
     def verify_request(self, request):
         self.verify_json(request.json, auth_spec)
 
-        auth_token = request.json["auth_token"]
-        self.user = Session.get_user(auth_token)
+        self.auth_token = request.json["auth_token"]
+        self.user = Session.get_user(self.auth_token)
+
+        del request.json["auth_token"]
 
         if self.user is None:
             raise AuthenticationException("Invalid auth_token")
