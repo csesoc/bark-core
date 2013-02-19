86a8eda022d054c7a477dac4d5d5ab0005e2666d
Vincent Tran <vintage@vintage.(none)>
2013-02-19 23:22:01 +1100

    Fix schema and bugs

--- bark/groups/models.py
+++ bark/groups/models.py
@@ -2,37 +2,31 @@ from bark import db
 from flask import jsonify
 from bark.users.models import User
 
-group_members = db.Table('group_members',
-    db.Column('group_id', db.Integer, db.ForeignKey('group.group_id'))
-    db.Column('user_id', db.Integer, db.ForeignKey('user.user_id'))
+group_members_associations = db.Table('group_members',
+    db.Column('group.id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('user.id', db.Integer, db.ForeignKey('groups.id'))
 )
 
-group_owners = db.Table('group_owners',
-    db.Column('group_id', db.Integer, db.ForeignKey('group.group_id'))
-    db.Column('user_id', db.Integer, db.ForeignKey('user.user_id'))
+group_owners_associations = db.Table('group_owners',
+    db.Column('group.id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('user.id', db.Integer, db.ForeignKey('groups.id'))
 )
 
 class Group(db.Model):
     __tablename__ = "groups"
 
-    group_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Text)
-    members = db.relationships("User", secondary=members, 
-        backref=db.backref('member_groups', lazy='dynamic'))
-    owners = db.relationships("User", secondary=owners, 
-        backref=db.backref('owned_groups', lazy='dynamic'))
-    name = db.Column(db.Text, unique=True)
-    description = db.Column(db.Text)
-    def __init__(self, name, members, owners, description):
-        self.name = name
-       
-        self.members = group_members
-        self.members = self.members.append(User(members))
-        self.members.execute()
-        
-        self.owners = group_owners
-        self.owners = self.owners.append(User(owners))
-        self.owners.execute()
-        
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    description = db.Column(db.String)
+    members = db.relationship("User", secondary=group_members_associations)
+    owners = db.relationship("User", secondary=group_owners_associations)
+    
+    def __init__(self, name, description):
         self.name = name
         self.description = description
+        
+        def add_member(self, user):
+            self.owners.append(user)
+
+        def add_owner(self, user):
+            self.owners.append(user)
