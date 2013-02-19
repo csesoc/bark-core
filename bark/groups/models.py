009aaa9e1b9429015c25599b3350b6e85b81e1d6
Vincent Tran <vintage@vintage.(none)>
2013-02-20 07:20:11 +1100

    Fix table names

--- bark/groups/models.py
+++ bark/groups/models.py
@@ -2,14 +2,14 @@ from bark import db
 from flask import jsonify
 from bark.users.models import User
 
-group_members_associations = db.Table('group_members',
-    db.Column('group.id', db.Integer, db.ForeignKey('users.id')),
-    db.Column('user.id', db.Integer, db.ForeignKey('groups.id'))
+group_members_associations = db.Table('group_members_associations',
+    db.Column('group_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('groups.id'))
 )
 
-group_owners_associations = db.Table('group_owners',
-    db.Column('group.id', db.Integer, db.ForeignKey('users.id')),
-    db.Column('user.id', db.Integer, db.ForeignKey('groups.id'))
+group_owners_associations = db.Table('group_owners_associations',
+    db.Column('group_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('groups.id'))
 )
 
 class Group(db.Model):
@@ -25,8 +25,8 @@ class Group(db.Model):
         self.name = name
         self.description = description
         
-        def add_member(self, user):
-            self.owners.append(user)
+    def add_member(self, user):
+        self.owners.append(user)
 
-        def add_owner(self, user):
-            self.owners.append(user)
+    def add_owner(self, user):
+        self.owners.append(user)
