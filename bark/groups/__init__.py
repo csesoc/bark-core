e9c1b274f958c4f48dbdb580623bfe0766b9d2dc
Vincent Tran <vintage@vintage.(none)>
2013-02-19 23:23:09 +1100

    Fix url rules and imports

--- bark/groups/__init__.py
+++ bark/groups/__init__.py
@@ -1,15 +1,15 @@
 from flask import Blueprint
 
-from .groups import *
+from .views import CreateGroupView, GroupView
 
 bp_groups = Blueprint("bp_groups", __name__)
 
 bp_groups.add_url_rule(
-    "/groups"
-    view_func=CreateGroupView.as_view("groups")
+    "/groups",
+    view_func=CreateGroupView.as_view("groups"),
     methods=["POST"])
 
 bp_groups.add_url_rule(
-    "/groups/<int: group_id>"
-    view_func=GroupView.as_view("groups")
-    methods=["POST"])
+    "/groups/<int: group_id>",
+    view_func=GroupView.as_view("groups"),
+    methods=["GET, DELETE"])
