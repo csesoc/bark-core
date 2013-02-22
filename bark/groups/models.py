147a587281fdadd4835938fc56c74d9a5eb7b624
Christopher Manouvrier <cmanou@mac.com>
2013-02-23 10:10:23 +1100

    Add Association object for student group membership

--- bark/groups/models.py
+++ bark/groups/models.py
@@ -1,16 +1,26 @@
 from bark import db
 from flask import jsonify
 from bark.users.models import User
+from sqlalchemy.ext.associationproxy import association_proxy
 
-group_members_associations = db.Table('group_members_associations',
-    db.Column('group_id', db.Integer, db.ForeignKey('groups.id')),
-    db.Column('user_id', db.Integer, db.ForeignKey('students.id'))
-)
+
 
 group_owners_associations = db.Table('group_owners_associations',
     db.Column('group_id', db.Integer, db.ForeignKey('groups.id')),
     db.Column('user_id', db.Integer, db.ForeignKey('users.id'))
 )
+
+class Membership(db.Model):
+    __tablename__ = 'group_members_associations'
+
+    id = db.Column('id', db.Integer, primary_key=True),
+    student_id = db.Column(db.Integer, db.ForeignKey('students.id')),
+    group_id = db.Column(db.Integer, db.ForeignKey('groups.id')),
+    expiry = db.Column(db.DateTime, nullable=False),
+
+    group = db.relationship(Group, backref="memberships")
+    student = db.relationship(Student, backref="memberships")
+
 
 class Group(db.Model):
     __tablename__ = "groups"
@@ -18,7 +28,7 @@ class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
     description = db.Column(db.String)
-    members = db.relationship("Student", secondary=group_members_associations)
+    members = association_proxy("memberships", "students")
     owners = db.relationship("User", secondary=group_owners_associations)
     
     def __init__(self, name, owner, description=''):
