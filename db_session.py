25edd1ac4c4825092f1f5c0e8cb86c0e9113fc83
Matthew Moss <mdm@cse.unsw.edu.au>
2013-02-23 06:45:38 -0800

    Add Device

--- db_session.py
+++ db_session.py
@@ -5,5 +5,6 @@ from bark.groups.models import Group
 from bark.persons.models import Person
 from bark.events.models import Event
 from bark.swipe.models import Swipe
+from bark.devices.models import Device
 
 create_app().test_request_context().push()
