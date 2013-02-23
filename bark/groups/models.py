bf3d83825219ac5acc442f639dd22faacc2e5d56
Matthew Moss <mdm@cse.unsw.edu.au>
2013-02-23 02:04:13 -0800

    Fix group associations

--- bark/groups/models.py
+++ bark/groups/models.py
@@ -3,8 +3,6 @@ from flask import jsonify
 from bark.users.models import User
 from sqlalchemy.ext.associationproxy import association_proxy
 
-
-
 group_owners_associations = db.Table('group_owners_associations',
     db.Column('group_id', db.Integer, db.ForeignKey('groups.id')),
     db.Column('user_id', db.Integer, db.ForeignKey('users.id'))
@@ -13,14 +11,11 @@ group_owners_associations = db.Table('group_owners_associations',
 class Membership(db.Model):
     __tablename__ = 'group_members_associations'
 
-    id = db.Column('id', db.Integer, primary_key=True),
-    student_id = db.Column(db.Integer, db.ForeignKey('students.id')),
-    group_id = db.Column(db.Integer, db.ForeignKey('groups.id')),
-    expiry = db.Column(db.DateTime, nullable=False),
-
-    group = db.relationship(Group, backref="memberships")
-    student = db.relationship(Student, backref="memberships")
-
+    expiry = db.Column(db.DateTime, nullable=False)
+    group_id = db.Column(db.Integer, db.ForeignKey('groups.id'), primary_key=True)
+    group = db.relationship('Group',)
+    student_id = db.Column(db.Integer, db.ForeignKey('students.id'), primary_key=True)
+    student = db.relationship('Student',)
 
 class Group(db.Model):
     __tablename__ = "groups"
@@ -28,13 +23,12 @@ class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
     description = db.Column(db.String)
-    members = association_proxy("memberships", "students")
     owners = db.relationship("User", secondary=group_owners_associations)
+    members = db.relationship("Membership")
     
-    def __init__(self, name, owner, description=''):
+    def __init__(self, name, description=''):
         self.name = name
         self.description = description
-        self.owners.append(owner)
         
     def add_member(self, student):
         self.members.append(student)
