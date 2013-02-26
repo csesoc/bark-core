07e7a75de713a518b7b899f011285dc0f444ef41
Christopher Manouvrier <cmanou@mac.com>
2013-02-26 15:34:49 +1100

    Convert Event Views to use json api

--- bark/events/views.py
+++ bark/events/views.py
@@ -4,7 +4,7 @@ from bark import db
 from .models import Event
 from bark.auth.shared import BarkAuthenticatedApiEndpoint
 from bark.groups.models import Group
-from bark.lib import time
+from bark.lib import time,api
 
 class EventView(BarkAuthenticatedApiEndpoint):
     required_fields_ = {
@@ -21,10 +21,9 @@ class EventView(BarkAuthenticatedApiEndpoint):
         owned_group_ids = [g.id for g in self.user.owned_groups]
         events = Event.query.filter(Event.group_id.in_(owned_group_ids))
         events_json = [e.to_json() for e in events.all()]
-        return {
-            "status": "OK",
-            'events': events_json
-        }
+        return api.json_ok({
+            "events": events_json
+        })
 
     def post(self, json):
         # User set by AuthenticatedApiEndpoint
@@ -41,15 +40,14 @@ class EventView(BarkAuthenticatedApiEndpoint):
             db.session.add(event)
             db.session.commit()
 
-            return {
-                "status": "OK",
-                "event_id": event.id,
-            }
+            return api.json_ok({
+                "event_id": event.id
+            })
         else:
-            return {
-                "status": "REQUEST_DENIED",
-                "error_detail": "User not member of group",
-            }
+            return api.json_error(
+                "REQUEST_DENIED",
+                "User not member of group"
+            )
             
 class SingleEventView(BarkAuthenticatedApiEndpoint):
 
@@ -58,15 +56,13 @@ class SingleEventView(BarkAuthenticatedApiEndpoint):
         if event is not None:
             group = Group.query.get(event.group_id)
             if group and self.user in group.owners:
-                return {
-                    "status": "OK",
-                    "event": event.to_json(),
-                }
-
-        return {
-            "status": "RESOURCE_ERROR",
-            "error_detail": "The requested event could not be found",
-        }
+                return api.json_ok({
+                    "event": event.to_json()
+                })
+        return api.json_error(
+            "RESOURCE_ERROR",
+            "The requested event could not be found"
+        )
         
     def delete(self, event_id=None):
         event = Event.query.get(event_id)
@@ -75,15 +71,12 @@ class SingleEventView(BarkAuthenticatedApiEndpoint):
             if self.user in group.owners:
                 db.session.delete(event)
                 db.session.commit()
+                return api.json_ok()
 
-                return {
-                    "status": "OK",
-                }
-
-        return {
-            "status": "RESOURCE_ERROR",
-            "error_detail": "The requested event could not be found",
-        }
+        return api.json_error(
+            "RESOURCE_ERROR",
+            "The requested event could not be found"
+        )
 
 class EventInfoView(BarkAuthenticatedApiEndpoint):
     def get(self, event_id=None):
@@ -91,13 +84,12 @@ class EventInfoView(BarkAuthenticatedApiEndpoint):
         if event is not None:
             group = Group.query.get(event.group_id)
             if group and self.user in group.owners:
-                return {
-                    "status": "OK",
+                return api.json_ok({
                     "event": event.to_json(),
                     "swipes": [s.to_json for s in event.swipes]
-                }
+                })
 
-        return {
-            "status": "RESOURCE_ERROR",
-            "error_detail": "The requested event could not be found",
-        }
+        return api.json_error(
+            "RESOURCE_ERROR",
+            "The requested event could not be found",
+        )
