054b0eebc2d1cb3c76d38643b50a930e919955ad
Christopher Manouvrier <cmanou@mac.com>
2013-03-01 16:27:26 +1100

    Undid silly

--- bark/events/models.py
+++ bark/events/models.py
@@ -16,7 +16,7 @@ class Event(db.Model):
     end_time = db.Column(db.DateTime)
 
     devices = db.relationship("Device")
-    swipes = association_proxy("devices", "swipes",uselist=False)
+    swipes = association_proxy("devices", "swipes")
 
     def __init__(self, group, name, description, start_time, end_time):
         self.group = group
